539c691610b21b3f989819508e176c58cfb555f1
yshzjq <ysh10150@gmail.com>
2024-11-13 23:07:59 +0900

    .

---
+++ AlgorithmStart/A+B - 2/A+B-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* cin cout 의 실행속도가 빨라지는 코드*/ {
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		cout.tie(NULL);
+	}
+
+	int A, B;
+
+	cin >> A;
+	cin >> B;
+
+	cout << A + B;
+
+}
---
+++ AlgorithmStart/A+B -3/A+B-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* cin cout 의 실행속도가 빨라지는 코드*/ {
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		cout.tie(NULL);
+	}
+
+	int A, B, T;
+
+	cin >> T;
+
+	while (T--)
+	{
+		cin >> A >> B;
+
+		cout << A + B << '\n';
+	}
+
+}
