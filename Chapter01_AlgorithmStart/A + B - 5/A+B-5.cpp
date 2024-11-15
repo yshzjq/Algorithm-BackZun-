183c17f7576c7b1f86d9be02a4725a68d9b0fa54
yshzjq <ysh10150@gmail.com>
2024-11-15 10:48:43 +0900

    Chapter01_Complete

---
+++ Chapter01_AlgorithmStart/A + B - 5/A+B-5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* cin cout 의 실행속도가 빨라지는 코드*/ {
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		cout.tie(NULL);
+	}
+
+	int A, B;
+
+	while (cin >> A >> B) // 성공적으로 입력 받을시 true
+	{
+		if (A == 0 && B == 0) break;
+		cout << A + B << '\n';
+	}
+
+	return 0;
+}
