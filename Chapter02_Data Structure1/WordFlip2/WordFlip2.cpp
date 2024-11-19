45cc739799bee8a6a36d87f9c388a98edbbd90d5
yshzjq <ysh10150@gmail.com>
2024-11-19 20:30:19 +0900

    Chapter02_solving problems

---
+++ Chapter02_Data Structure1/WordFlip2/WordFlip2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>   
+
+using namespace std;
+
+int main()
+{
+    string str;
+    string result;
+
+    bool isSign = false;
+
+    stack<char> s;
+
+    getline(cin,str);
+
+    for (char c : str)
+    {
+        if (isSign)
+        {
+            if (c == '>')
+            {
+                isSign = false;
+            }
+            result += c;
+        }
+        else
+        {
+            if (c == '<')
+            {
+                isSign = true;
+
+                while (!s.empty())
+                {
+                    result += s.top();
+                    s.pop();
+                }
+
+                result += c;
+            }
+            else if (c == ' ')
+            {
+                while (!s.empty())
+                {
+                    result += s.top();
+                    s.pop();
+                }
+                result += c;
+            }
+            else
+            {
+                s.push(c);
+            }
+        }
+    }
+
+    while (!s.empty())
+    {
+        result += s.top();
+        s.pop();
+    }
+
+    cout << result;
+}
