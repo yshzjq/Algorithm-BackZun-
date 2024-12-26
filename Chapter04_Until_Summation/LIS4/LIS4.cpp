c32c5464ccb40b7408f306fbec64bc2bcdc37c1e
yshzjq <ysh10150@gmail.com>
2024-12-27 00:41:25 +0900

    Chapter04_Until_Summation(Solving_Problems)

--- Chapter04_Until_Summation/LIS4/LIS4.cpp
+++ Chapter04_Until_Summation/LIS4/LIS4.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int A[1001];
 int D[1001];
+int V[1001];
+
+void f(int p)
+{
+	if (p == 0) return;
+
+	f(V[p]);
+	cout << A[p] << ' ';
+}
 
 int main()
 {
 	int N;
+
+	stack<int> stk;
 
 	cin >> N;
 
@@ -16,33 +28,29 @@ int main()
 		cin >> A[i];
 	}
 
-	int max = 0;
-	D[max] = 1;
+	int max = 1;
 
 	for (int i = 1; i <= N; i++)
 	{
 		D[i] = 1;
+		V[i] = 0;
 		for (int j = 1; j < i; j++)
 		{
 			if (A[j] < A[i] && D[j] + 1 > D[i])
 			{
 				D[i] = D[j] + 1;
-				if (D[max] < D[i]) max = i;
+				V[i] = j;
+				if (D[max] < D[i])
+				{
+					max = i;
+				}
 			}
 		}
 	}
 
-	int tmp = 1;
+	cout << D[max] << '\n';
 
-	for (int i = 1; i <= N; i++)
-	{
-		if (tmp + 1 == D[i])
-		{
-			cout << A[i-1];
-			tmp++;
-		}
-	}
-
+	f(max);
 
 	return 0;
 }
