c6cc2e703b9a2fb5e5d14f9639b751376f024f65
yshzjq <ysh10150@gmail.com>
2025-02-17 00:57:16 +0900

    Chapter04_Dynamic_Programming(Practice)_Resolving

--- Chapter04_Dynamic_Programming(Practice)/wine_tasting/wine_tasting.cpp
+++ Chapter04_Dynamic_Programming(Practice)/wine_tasting/wine_tasting.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 int wine[10001];
-long long A[10001][16];
+int A[10001][3];
 
 int main()
 {
@@ -17,18 +17,20 @@ int main()
 	{
 		cin >> wine[i];
 	}
+
 	A[1][0] = 0;
-	A[1][1] = wine[0];
+	A[1][1] = wine[1];
 	A[1][2] = wine[0] + wine[1];
 
 
 	for (int i = 2;i <= n;++i)
 	{
-		A[i][0] = max({A[i - 1][2], A[i - 2][1],A[i-1][1]});
-		A[i][1] = A[i-1][0]+ wine[i];
+		A[i][0] = max({A[i - 1][2], A[i-1][0],A[i-1][1]});
+		A[i][1] = A[i-1][0] + wine[i];
 		A[i][2] = A[i-1][1] + wine[i];
 	}
 
 	cout << max({ A[n][0],A[n][1],A[n][2] }) << '\n';
+	return 0;
 
 }
